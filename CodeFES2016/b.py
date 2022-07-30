d59edd1b33281deb8313c5cbeb973e06d9c4f231
Eggngineer <yama_shin1999@icloud.com>
2022-07-30 13:31:57 +0900

    some atcoder problems are done

--- CodeFES2016/b.py
+++ CodeFES2016/b.py
@@ -1,4 +1,29 @@
 
+from collections import defaultdict
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
+
+
+d = defaultdict(int)
+
+n = int(input())
+a = list(map(int, input().split()))
+rabbits = set(a)
+
+for i in range(n):
+    d[i+1] = a[i]
+
+ans = 0
+
+for rabbit in list(range(1, n+1)):
+    if rabbit in rabbits:
+        rabbits.remove(rabbit)
+        if d[rabbit] in rabbits:
+            if d[d[rabbit]] == rabbit:
+                rabbits.remove(d[rabbit])
+                ans += 1
+    if not rabbits:
+        break
+
+print(ans)
